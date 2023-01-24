9000b01097291cc9e170273475f005ec86e49534
denizsult <hallobro82@gmail.com>
2023-01-24 23:30:16 +0300

    initial cımmit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="link"> ANAN </Button>
+    </div>
+  );
+}
+
+export default App;
